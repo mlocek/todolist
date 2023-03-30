931f9df4c6673af14365ae781eae00c8ac758971
mlocek <mlocekdev@gmail.com>
2023-03-30 19:20:52 +0200

    optimized write todos

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def get_todos(filepath="todos.txt"):
         todos = file.readlines()
     return todos
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="todos.txt"):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
@@ -22,7 +22,7 @@ while True:
 
         todos.append(todo + '\n')
 
-        write_todos("todos.txt", todos)
+        write_todos(todos)
 
     elif user_action.startswith("show"):
 
@@ -46,7 +46,7 @@ while True:
             new_todo = input("Enter new todo:")
             todos[number] = new_todo + '\n'
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
         except ValueError:
             print("Your command is not valid")
             continue
@@ -61,7 +61,7 @@ while True:
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
