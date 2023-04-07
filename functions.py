6154ff1b0059290d418175e2cb9666c5245dcfa4
mlocek <mlocekdev@gmail.com>
2023-04-07 12:51:59 +0200

    included functions in separate file

---
+++ functions.py
@@ -0,0 +1,12 @@
+def get_todos(filepath="todos.txt"):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    """ Write the to-do item list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-todos = []
-
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list of
-    to-do items.
-    """
-    with open(filepath, 'r') as file:
-        todos = file.readlines()
-    return todos
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """ Write the to-do item list in the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
+from functions import get_todos, write_todos
 
 while True:
     user_action = input("Type add, edit, complete, show or exit: ")
